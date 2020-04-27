f6fb87e83f2a8856919583c3fab92fa28d82e446
Jack Skiles <jackskiles420@gmail.com>
2020-04-27 18:35:40 -0400

    changes

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 
 const name = document.getElementById("username");
+const password = document.getElementById("password");
+const submit = document.getElementById("submit");
+const email = document.getElementById("email");
+const aboutMe = document.getElementById("info");
 
 function checkLength() {
     if (name.value.length >= 15) {
         alert("Please keep it less than 15");
     }
 }
+
+function checkPassword() {
+    if (password.value.length < 6) {
+        alert("Please enter a longer password");
+    }
+}
+
+function checkEmail() {
+    if (email.value == False) {
+        alert("Please enter valid email address");
+    }
+}
+function checkInfo() {
+    if (aboutMe.value.length === 250) {
+        alert("Please Shorten your entry.")
+    }
+}
+
+submit.addEventListener("click", checkEmail);
+submit.addEventListener("click", checkPassword);
 name.addEventListener("input", checkLength);
+aboutMe.addEventListener("input", checkInfo);
